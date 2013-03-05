75e44afd3c1582d73b4ca5fb3fe6e65ed692ef5c
AndrewCEmil <andrew.emil@10gen.com>
2013-03-04 22:59:12 -0800

    started playing around with R codes

---
+++ pca_rfb.R
@@ -0,0 +1,23 @@
+library(randomForest)
+
+train <- read.csv("train.csv", header=TRUE)
+test <- read.csv("test.csv", header=TRUE)
+
+labels <- as.factor(train[,1])
+train <- train[,-1]
+print(dim(train))
+print(typeof(train))
+print(names(train))
+
+fit <- princomp(train)
+for(i in 1:length(fit$sdev)) {
+    if(fit$sdev[i] == 0) {
+        train <- train[,-i] 
+
+print(dim(train))
+
+
+rf <- randomForest(train, labels, xtest=test, ntree=1000)
+predictions <- levels(labels)[rf$test$predicted]
+
+write(predictions, file="rf_benchmark.csv", ncolumns=1) 
