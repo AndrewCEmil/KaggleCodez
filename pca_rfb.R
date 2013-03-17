71ed9636871076c7eb2eabb4d33fb9d691cf87d9
AndrewCEmil <andrew.emil@10gen.com>
2013-03-16 23:32:12 -0700

    got the column reversal going

--- pca_rfb.R
+++ pca_rfb.R
@@ -5,22 +5,33 @@ test <- read.csv("test.csv", header=TRUE)
 
 labels <- as.factor(train[,1])
 train <- train[,-1]
-print(dim(train))
-print(typeof(train))
-
 fit <- princomp(train)
-for(i in 1:length(fit$sdev)) {
-    if(fit$sdev[i] == 0) {
-        train <- train[,-i] 
-        test <- test[,-i]
-    }
-}
 
-
+print(sum(sorted[1:420]))
+print(sum(sorted[420:length(sorted)]))
 print(dim(train))
+#realtrain <- train[names(sorted[420:length(sorted)])]
+#for(name in names(sorted[420:length(sorted)])) {
+#    train[,name] <- NULL
+#}
+#for i in length(sdev)
+    #if sdev[i] < sorted[lim]
+        #train[,i] <- null
 
+train <- train[420:length(train)]
+print(dim(train))
+print(dim(test))
+test <- test[420:length(test)]
+print(dim(test))
+#for(i in 1:length(fit$sdev)) {
+#    if(fit$sdev[i] <= sorted[420]) {
+#        train <- train[,-i]
+#        test <- test[,-i]
+#        count[1] <- count[1] + 1
+#    }
+#}
 
 rf <- randomForest(train, labels, xtest=test, ntree=1000)
 predictions <- levels(labels)[rf$test$predicted]
 
-write(predictions, file="rf_benchmark.csv", ncolumns=1) 
+write(predictions, file="rf_pca.csv", ncolumns=1) 
