f154a9ffd630559fd05dedb956e98c7e965c3e91
AndrewCEmil <andrew.emil@10gen.com>
2013-04-06 16:49:16 -0700

    Made some more work in buldozer

--- BULDOZER/rfTest.R
+++ BULDOZER/rfTest.R
@@ -9,27 +9,28 @@ targets <- train[,"SalePrice"]
 #targets <- as.factor(train[,"SalePrice"])
 print(dim(train))
 print(dim(test))
-goodHeaders <- c("YearMade", "MachineID", "ModelID", "auctioneerID", "MachineHoursCurrentMeter")
-realtrain <- train[,goodHeaders]
-realtest <- test[,goodHeaders]
+goodHeaders <- c("YearMade", "MachineID", "ModelID", "auctioneerID", "MachineHoursCurrentMeter", "saledate")
+train <- train[,goodHeaders]
+test <- test[,goodHeaders]
 
-realtrain[realtrain != realtrain] <- 0 #removes the NAs
-realtrain[is.na(realtrain)] <- 0#also removes the NAs lol
-realtest[realtest != realtest] <- 0
-realtest[is.na(realtest)] <- 0
+train[,"saledate"] <- as.Date(train[,"saledate"],"%m/%d/%Y")
+test[,"saledate"] <- as.Date(test[,"saledate"], "%m/%d/%Y")
+train[train != train] <- 0 #removes the NAs
+train[is.na(train)] <- 0#also removes the NAs lol
+test[test != test] <- 0
+test[is.na(test)] <- 0
 targets[is.na(targets)] <- 0
 targets[targets != targets] <- 0
+train[,"YearMade"] <- as.Date(train[,"YearMade"],"%Y")
+test[,"YearMade"] <- as.Date(test[,"YearMade"],"%Y")
+
 #targs <- na.omit(targets)
-#realtrain <- na.omit(realtrain)
-#realtest <- na.omit(realtest)
+#train <- na.omit(train)
+#test <- na.omit(test)
 
-realtrain2 <- realtrain[100000:length(targs2),]
-targs2 <- targs[100000:length(targs2)]
-print(length(targs2))
-print(dim(realtrain2))
-rf <- randomForest(realtrain, targets, ntree=10)
+#train2 <- train[100000:length(targs2),]
+#targs2 <- targs[100000:length(targs2)]
+rf <- randomForest(train, targets, ntree=10)
 #rf2 <- randomForest(realtrain2, targs2, ntree=1000)
-results <- predict(rf, realtest)
-predictions <- results
-
-write(predictions, file="sub3.csv", ncolumns=1)
+results <- predict(rf, test)
+write(results, file="sub5.csv", ncolumns=1)
